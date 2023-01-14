c1e64949fabb0fad04d995e9d954d05c08572cde
iosip <iosipoi@gmail.com>
2023-01-14 13:21:09 +0200

    Add project files.

---
+++ GeneralLibrary/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GeneralLibrary
+{
+    public class Person
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{id}. {Name} - {Age}";
+        }
+    }
+}
