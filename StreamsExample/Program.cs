3f00d9c186723a337d43f5bbce90b163f2914e57
iosip <iosipoi@gmail.com>
2023-01-21 11:47:56 +0200

    add regex

---
+++ StreamsExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace StreamsExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = "log.txt";
+            using (FileStream fs = new FileStream(filePath,FileMode.OpenOrCreate))
+            {
+                //BinaryWriter bw = new BinaryWriter(fs);
+                //string inputText = Console.ReadLine();
+
+                //int number = 1235;
+
+                //bw.Write(inputText);
+                //bw.Write(number);
+
+
+                RegexExample.EmailPattern();
+
+
+
+            }
+        }
+
+
+    }
+}
---
+++ StreamsExample/RegexExample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StreamsExample
+{
+    internal class RegexExample
+    {
+        public static void EmailPattern()
+        {
+            string emailPattern = "[a-z0-9_-]+@[a-z0-9_-]+.[a-z]";
+
+            string email = Console.ReadLine();
+            Regex regex = new Regex(emailPattern);
+            Console.WriteLine(regex.IsMatch(email));
+        } 
+    }
+}
